4d8ee49a946539f15113459b2aa36cd860857c21
Flown <105561081+super-miner@users.noreply.github.com>
2024-10-25 20:20:27 -0700

    Added the rest of the motion blocks to the opcode table

--- src/opcodetable.ts
+++ src/opcodetable.ts
@@ -21,5 +21,15 @@ export const opcodeTable: {[id: string]: FieldData} = {
     'motion_gotoxy': {inputs: [{name: 'X', reference: null}, {name: 'Y', reference: null}], fields: []},
     'motion_glideto': {inputs: [{name: 'SECS', reference: null}, {name: 'TO', reference: 'motion_glideto_menu'}], fields: []},
     'motion_glideto_menu': {inputs: [], fields: [{name: 'TO'}]},
+    'motion_glidesecstoxy': {inputs: [{name: 'SECS', reference: null}, {name: 'X', reference: null}, {name: 'Y', reference: null}], fields: []},
+    'motion_pointindirection': {inputs: [{name: 'DIRECTION', reference: null}], fields: []},
+    'motion_pointtowards': {inputs: [{name: 'TOWARDS', reference: 'motion_pointtowards_menu'}], fields: []},
+    'motion_pointtowards_menu': {inputs: [], fields: [{name: 'TOWARDS'}]},
+    'motion_changexby': {inputs: [{name: 'DX', reference: null}], fields: []},
+    'motion_setx': {inputs: [{name: 'X', reference: null}], fields: []},
+    'motion_changeyby': {inputs: [{name: 'DY', reference: null}], fields: []},
+    'motion_sety': {inputs: [{name: 'Y', reference: null}], fields: []},
+    'motion_ifonedgebounce': {inputs: [], fields: []},
+    'motion_setrotationstyle': {inputs: [], fields: [{name: 'STYLE'}]},
     'event_whenflagclicked': {inputs: [], fields: []}
 };
