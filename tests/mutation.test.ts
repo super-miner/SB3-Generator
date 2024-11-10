fff5197c2a94eb228ee08dc843b9c82708166e67
Flown <105561081+super-miner@users.noreply.github.com>
2024-11-09 21:54:12 -0800

    Added tests for custom blocks

--- tests/mutation.test.ts
+++
@@ -1,9 +0,0 @@
-import { Mutation } from "../src/mutation";
-
-test('Creating mutation correctly', () => {
-    const mutation = new Mutation([], false);
-
-    expect(mutation.tagName).toBe('mutation');
-    expect(mutation.children).toStrictEqual([]);
-    expect(mutation.hasnext).toBe(false);
-});
