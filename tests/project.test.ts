f405f5b706227b5bbc4dfc9d50cad584a27eb141
Marshall Derksen <theredmugproductions@gmail.com>
2025-06-24 12:36:45 -0700

    Updated tests to use beforeEach

--- tests/project.test.ts
+++ tests/project.test.ts
@@ -1,8 +1,12 @@
 import { Project } from "../src/project";
 import { createSprite } from "../src/sb3Generator";
 
+let project: Project;
+
 beforeEach(() => {
     jest.spyOn(global.Math, 'random').mockReturnValue(0.0);
+
+    project = new Project('Test project');
 });
 
 afterEach(() => {
@@ -10,18 +14,15 @@ afterEach(() => {
 });
 
 test('Constructor assigns values correctly', () => {
-    const project = new Project('Test project');
     expect(project.name).toBe('Test project');
 });
 
 test('Correctly adds sprite', () => {
-    const project = new Project('Test Project');
     project.withSprite(createSprite('Test sprite 1'));
     expect(project.targets).toStrictEqual([createSprite('Test sprite 1')]);
 });
 
 test('Correctly adds multiple sprites', () => {
-    const project = new Project('Test Project');
     project.withSprite(createSprite('Test sprite 1'));
     project.withSprite(createSprite('Test sprite 2'));
     expect(project.targets).toStrictEqual([createSprite('Test sprite 1'), createSprite('Test sprite 2')]);
@@ -30,11 +31,9 @@ test('Correctly adds multiple sprites', () => {
 //TODO: Need tests for build.
 
 test('Correct output directory path', () => {
-    const project = new Project('Test Project');
     expect(project.getOutputDirectoryPath()).toBe('output/');
 });
 
 test('Correct output file path', () => {
-    const project = new Project('Test Project');
     expect(project.getOutputFilePath()).toBe('output/Test Project.sb3');
 });
--- tests/sound.test.ts
+++ tests/sound.test.ts
@@ -2,8 +2,13 @@ import fs from 'fs';
 import { md5 } from 'js-md5';
 import { Sound } from '../src/sound';
 
+let sound: Sound;
+
+beforeEach(() => {
+    sound = new Sound('TestSound.wav');
+});
+
 test('Constructor assigning values correctly', () => {
-    const sound = new Sound('TestSound.wav');
     expect(sound._path).toBe('TestSound.wav');
     expect(sound._data).toStrictEqual(fs.readFileSync('TestSound.wav'));
     expect(sound.name).toBe('TestSound');
