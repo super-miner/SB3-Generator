22c83d7def9e9afabd6735a9114f62d94c43b828
Marshall Derksen <theredmugproductions@gmail.com>
2025-07-06 11:24:10 -0700

    Updated List tests

--- tests/list.test.ts
+++ tests/list.test.ts
@@ -19,34 +19,39 @@ test('Constructor assigning values correctly', () => {
     expect(list.values).toStrictEqual([]);
 });
 
+test('Adding single string value correctly', () => {
+    list.withValue('Hello');
+    expect(list.values).toStrictEqual(['Hello']);
+});
+
 test('Adding single number value correctly', () => {
     list.withValue(10);
-    expect(list.values).toStrictEqual([10]);
+    expect(list.values).toStrictEqual(['10']);
 });
 
 test('Adding multiple single number values correctly', () => {
     list.withValue(9);
     list.withValue(8);
-    expect(list.values).toStrictEqual([9, 8]);
+    expect(list.values).toStrictEqual(['9', '8']);
 });
 
 test('Adding multiple number values correctly', () => {
     list.withValues([100, 110]);
-    expect(list.values).toStrictEqual([100, 110]);
+    expect(list.values).toStrictEqual(['100', '110']);
 });
 
-test('Adding single string value correctly', () => {
-    list.withValue('hello');
-    expect(list.values).toStrictEqual(['hello']);
+test('Adding single true boolean value correctly', () => {
+    list.withValue(true);
+    expect(list.values).toStrictEqual(['true']);
 });
 
-test('Adding single boolean value correctly', () => {
+test('Adding single false boolean value correctly', () => {
     list.withValue(false);
-    expect(list.values).toStrictEqual([false]);
+    expect(list.values).toStrictEqual(['false']);
 });
 
 test('Adding multiple any type values correctly', () => {
     list.withValues([true, 'testing']);
     list.withValue(69);
-    expect(list.values).toStrictEqual([true, 'testing', 69]);
+    expect(list.values).toStrictEqual(['true', 'testing', '69']);
 });
